7ae870a026722e474eb40b95c8dde2e5391d006f
Fernanda <mangorifernanda@gmail.com>
2023-07-29 20:02:59 +0800

    home screen

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,26 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />
+    const [fontsLoaded] = useFonts({
+        DMBold: required('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: required('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: required('../assets/fonts/DMSans-Regular.ttf'),
+    })
+
+    const onLayoutRootView = useCallback(async () => {
+     if(fontsLoaded) {
+        await SplashScreen.hideAsync();
+     }   
+    }, [fontsLoaded])
+
+    if(!fontsLoaded) return null;
+
+    return <Stack onLayout={onLayoutRootView} />; 
 }
 
 export default Layout;
